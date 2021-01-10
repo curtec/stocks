7f7c9fac1a366da7027635afaae12146e8cdc0e9
Adam Curtis <adam@curtec.com.au>
2021-01-10 21:24:00 +1000

    final

--- stocksapp.py
+++ stocksapp.py
@@ -3,10 +3,10 @@ import streamlit as st
 import pandas as pd
 
 st.write("""
-# Simple Stock Price App
+# Simple Stock Price App Based on a YouTube Tutorial
 
- Shown are stock closing price and volume Tesla
-
+ Currently showing the closing price and volume Tesla but can be changed to any stock. 
+You can zoom in or out and simply double click to return to the starting point.
 """ )
 
 tickerSymbol = 'TSLA'
